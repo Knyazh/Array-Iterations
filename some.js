5f5a41c870c573032023c35477549a20e7d5424c
Hp <qnyazface@maul.ru>
2023-06-18 16:35:42 +0400

    some.js added

---
+++ some.js
@@ -0,0 +1,17 @@
+function findFunction(array){
+    let some = '';
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if(element>25){
+           some=true;
+           break;
+        }
+        else{
+            some=false;
+        }
+    }
+    return alert(some)
+}
+
+let numbers = [45,14,52,3,61,9,19]
+findFunction(numbers);
---
+++ sorting.js
@@ -0,0 +1,7 @@
+let wordArray = ['apple', 'abbot', 'aatrophy', 'banana', 'berry', 'cherrypie', 'cherry', 'candy', 'grapefruit', 'pear', 'pizza', 'zebra', 'cigarette', 'guitar'];
+let wordToLetterArray = [];
+let sortingString = "abcdefghijklmnopqrstuvwxyz";
+sortingArray=sortingString.split('');
+
+
+
