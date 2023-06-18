12fb54c2d98d801142c2a0eee86e976bac15493a
Hp <qnyazface@maul.ru>
2023-06-18 15:27:39 +0400

    array.map added

--- map.js
+++ map.js
@@ -1,10 +1,17 @@
-let numbers = [12, 45, 7, 53, 26, 2];
-let newNumbers;
-let elemen = '';
-
-for (let index = 0; index < numbers.length; index++) {
-    let element = numbers[index];
-    elementShow += element*2 + ', ';
-}
-
-alert(elementShow);
+function customMap(array) {
+    let mappedArray = [];
+  
+    for (let index = 0; index < array.length; index++) {
+      let element = array[index];
+      let transformedValue = element * 2;
+      mappedArray[index] = transformedValue;
+    }
+  
+    return mappedArray;
+  }
+  
+  let numbers = [12, 45, 7, 53, 26, 2];
+  let elementShow = customMap(numbers);
+  
+  let newNumbers = elementShow.join(', ');
+  alert(newNumbers);
